7d360d10c405c0da917d67b504f665e41c66d4f9
wepukka <anssi.vepsalainen1@gmail.com>
2024-01-13 15:45:04 +0200

    single this.value

--- src/components/MovieFilters.vue
+++ src/components/MovieFilters.vue
@@ -1,16 +1,5 @@
 <script setup>
-import { ref } from 'vue'
-
-// Emit
 defineEmits(['fetchWithFilters'])
-
-// Props
-const props = defineProps({
-  filters: Object
-})
-
-// REF //
-let filters = ref(props.filters)
 </script>
 
 <template>
@@ -29,15 +18,15 @@ let filters = ref(props.filters)
       type="number"
       maxlength="4"
     ></v-text-field>
-    <v-select v-model="filters.genre" :items="genres" label="Filter By Genre">
-      <template #selection="{ item }">
-        <v-chip class="v-chip" v-if="this.filters.genre !== ''">
-          {{ item.title }}
-        </v-chip>
-      </template>
-    </v-select>
-    <v-btn block style="color: var(--neg)" @click="clearFilters(filters)">Clear Filters</v-btn>
-    <v-btn block style="color: var(--pos)" @click="fetch(filters)">Search</v-btn>
+    <v-select
+      ref="myVSelect"
+      class="test"
+      v-model="filters.genre"
+      :items="genres"
+      label="Filter By Genre"
+      chips
+    ></v-select>
+    <v-btn block @click="fetch(filters)">Search</v-btn>
   </div>
 </template>
 
@@ -45,37 +34,27 @@ let filters = ref(props.filters)
 export default {
   data() {
     return {
-      genres: ['Drama', 'Fiction', 'Documentary']
+      genres: ['Drama', 'Fiction', 'Documentary'],
+      filters: {
+        genre: '',
+        year: '',
+        title: ''
+      }
     }
   },
   methods: {
     fetch(filters) {
       this.$emit('pushQueryAndReload', filters)
-    },
-    deleteItem(item) {
-      this.filters.genre = this.filters.genre.filter((find) => find !== item)
-    },
-    clearFilters() {
-      ;(this.filters.genre = ''), (this.filters.title = ''), (this.filters.year = '')
     }
   }
 }
 </script>
 
 <style scoped>
-.v-chip {
-  display: flex;
-
-  background-color: var(--item-background);
-}
 .container {
   display: flex;
   flex-direction: column;
   gap: 10px;
   margin-bottom: 20px;
-}
-
-v-chip {
-  background-color: red;
 }
 </style>
